0a5ac0c2c2c1f174833b580cb7546a986bf1725b
SolForte <mateusbrandao1234@gmail.com>
2023-05-22 04:55:38 -0300

    feat: open url by id

--- src/routers/urls.routes.js
+++ src/routers/urls.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/shorten.schema.js";
-import { getUrlById, shortenUrl } from "../controllers/urls.controller.js";
-
+import {
+  getUrlById,
+  openUrlById,
+  shortenUrl,
+} from "../controllers/urls.controller.js";
 
 const urlsRouter = Router();
 
-urlsRouter.post("/urls/shorten", validateSchema(urlSchema), shortenUrl)
-urlsRouter.get("/urls/:id", getUrlById)
+urlsRouter.post("/urls/shorten", validateSchema(urlSchema), shortenUrl);
+urlsRouter.get("/urls/:id", getUrlById);
+urlsRouter.get("/urls/open/:shortUrl", openUrlById);
 
 export default urlsRouter;
